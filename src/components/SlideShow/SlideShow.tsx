2b99a661d3803e16a6fbdd7c15805a488a930e5f
Amrit Bains <BainsA1@cardiff.ac.uk>
2022-11-27 21:30:30 +0000

    fixed logic for slideshow trying to access invalid story index after topic changed

--- src/components/SlideShow/SlideShow.tsx
+++ src/components/SlideShow/SlideShow.tsx
@@ -18,22 +18,26 @@ export const SlideShow: React.FC<SlideShow> = ({ stories, heading }) => {
   //   increment slide every 10s and return to heading slide every 10s
   useEffect(() => {
     const interval = setInterval(() => {
-      if (stories != null && index == stories.length) {
-        setIndex((prev) => 0);
-      } else {
-        setIndex((prev) => prev + 1);
+      if (stories != null) {
+        if (index == stories.length - 1) {
+          setIndex((prev) => 0);
+        } else {
+          setIndex((prev) => prev + 1);
+        }
       }
     }, 10000);
 
     return () => clearInterval(interval);
-  }, [index, stories]);
+  }, [index]);
 
   return (
     <div className="SlideShow">
       <div className="HeadingContainer">
         <div className="Heading">{heading}</div>
       </div>
-      {stories != null && index != stories.length ? (
+      {stories != null &&
+      index != stories.length &&
+      stories[index].title != null ? (
         <div className="Slide">
           <div className="SlideContent">
             <SlideContent
